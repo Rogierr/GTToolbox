b751b44c769d8c6a617438e690611b47b94abb33
Rogierr <rogierhar@hotmail.com>
2019-04-15 14:09:57 +0200

    Adjustment of plot_convex_hull_pure_rewards

--- Old/ETPGame.py
+++ Old/ETPGame.py
@@ -22,6 +22,15 @@ class ETPGame:
         self.transition_matrix_game1_to2 = trmatrixg3  # transition matrix from game 1 to game 2
         self.transition_matrix_game2_to2 = trmatrixg4  # transition matrix from game 2 to game 2
 
+        self.payoff_p1_g1_flat = self.payoff_p1_game1.A1  # store the flatten payoff of p1 game 1
+        self.payoff_p2_g1_flat = self.payoff_p2_game1.A1  # store the flatten payoff of p2 game 1
+
+        self.payoff_p1_g2_flat = self.payoff_p1_game2.A1  # store the flatten payoff of p1 game 2
+        self.payoff_p2_g2_flat = self.payoff_p2_game2.A1  # store the flatten payoff of p2 game 2
+
+        self.payoff_p1_merged = np.concatenate((self.payoff_p1_g1_flat, self.payoff_p1_g2_flat))  # merge p1 payoffs
+        self.payoff_p2_merged = np.concatenate((self.payoff_p2_g1_flat, self.payoff_p2_g2_flat))  # merge p2 payoffs
+
         self.etp_matrix = matrixA
 
         self.printing = False  # set printing to False
--- plotting/plot_convex_hull_pure_rewards.py
+++ plotting/plot_convex_hull_pure_rewards.py
@@ -1,16 +1,13 @@
+import numpy as np
+from scipy.spatial import ConvexHull  # import scipy convex hull package
+import matplotlib.pyplot as plt  # import package to plot stuff
+
+
 def plot_convex_hull_pure_rewards(self):
-    "Here we plot a convex hull around the pure reward point, therefore resulting in the total possible reward space"
+    """Here we plot a convex hull around the pure reward point, therefore resulting
+    in the total possible reward space"""
 
-    payoff_p1_g1_flat = self.payoff_p1_game1.A1  # store the flattend payoff of p1 game 1
-    payoff_p2_g1_flat = self.payoff_p2_game1.A1  # store the flattend payoff of p2 game 1
-
-    payoff_p1_g2_flat = self.payoff_p1_game2.A1  # store the flattend payoff of p1 game 2
-    payoff_p2_g2_flat = self.payoff_p2_game2.A1  # store the flattend payoff of p2 game 2
-
-    payoff_p1_merged = np.concatenate((payoff_p1_g1_flat, payoff_p1_g2_flat))  # merge p1 payoffs
-    payoff_p2_merged = np.concatenate((payoff_p2_g1_flat, payoff_p2_g2_flat))  # merge p2 payoffs
-
-    all_payoffs = np.array([payoff_p1_merged, payoff_p2_merged])  # create one array of payoffs
+    all_payoffs = np.array([self.payoff_p1_merged, self.payoff_p2_merged])  # create one array of payoffs
     all_payoffs = np.transpose(all_payoffs)  # and rotate this one
 
     rewards_convex_hull = ConvexHull(all_payoffs)  # retain the convex hull of the payoffs
