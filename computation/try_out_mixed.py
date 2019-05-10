22b092f2c59899491c0bb0ab6cfe2c7bea2e8912
Rogierr <rogierhar@hotmail.com>
2019-05-10 14:22:26 +0200

    Try out developed with mixing

--- computation/try_out_mixed.py
+++ computation/try_out_mixed.py
@@ -11,10 +11,8 @@ def mixed_strategy_try_out():
 
     x = np.zeros(8)
 
-    # print(p1)
-    # print(p2)
-
     c = 0
+
     for i in range(0, 2):
         for j in range(0, 2):
             x[c] = np.multiply(p1_1[:, i], p2_1[:, j])
@@ -25,9 +23,8 @@ def mixed_strategy_try_out():
             x[c] = np.multiply(p1_2[:, i], p2_2[:, j])
             c += 1
 
+    x = (x/2)
+
     print(x)
-    # print(np.sum(p1))
-    # print(np.sum(p2))
-    print(np.sum(x))
 
 mixed_strategy_try_out()
