cb8ca6767b77cca82e4e0e3c008e60151eb29934
Rogierr <rogierhar@hotmail.com>
2019-10-03 22:12:19 +0200

    Last update to the agent examples, now fully commented and ready.

--- agents/agent_prisoners.py
+++ agents/agent_prisoners.py
@@ -1,158 +1,291 @@
+# necessary packages
 import time
 import numpy as np
 from osbrain import run_nameserver
 from osbrain import run_agent
 from osbrain import Agent
 
+# here below we define the payoffs for player 1 and 2 (prisoners game)
 game_p1 = np.array([[-1, -10], [0, -9]])
 game_p2 = np.array([[-1, 0], [-10, -9]])
 
 
 class Alice(Agent):
     def on_init(self):
+        """
+        Initialize Alice as an agent with two bindings
+        :return:
+        """
         self.bind('PUSH', alias='main')
         self.bind('PUSH', alias='strategy')
 
     def hello(self, name):
+        """The hello message from alice"""
         self.send('main', 'Hello to %s from Alice!' % name)
 
     def custom_log(self, message):
+        """
+        Standard log that prints a message
+        :param message: message that needs to be stated
+        :return:
+        """
         self.log_info('Received a message: %s' % message)
 
     def nash_strategy(self):
+        """
+        Function that sets the strategy to a nash strategy
+        :return:
+        """
         self.strategy = 1
 
     def random_strategy(self):
-        upper_row = np.random.uniform()
+        """
+        Play a random strategy if this one is chosen
+        :return:
+        """
+        upper_row = np.random.uniform() # draw a random number from uniform distribution
 
+        # if the number is higher than 0.5, we play the upper row, else we play the bottom row
         if upper_row > 0.5:
             self.strategy = 0
         else:
             self.strategy = 1
 
     def cooperate_strategy(self):
+        """
+        Set the strategy to be played to cooperate
+        :return:
+        """
         self.strategy = 0
 
     def receive_strategy(self, strategy):
+        """
+        Function that receives the strategy of the other player and stores it
+        :param strategy: opponents strategy
+        :return:
+        """
         self.opponent_strategy = strategy
 
     def tit_for_tat_strategy(self, round_number):
+        """
+        Play a tit for tat strategy
+        :param round_number: The current round we are in with the game
+        :return:
+        """
+
+        # if we are in the first round, we will always cooperate
         if round_number == 1:
             self.strategy = 0
         else:
+            # if not, we base our strategy on the opponents last chosen strategy
             if self.opponent_strategy == 1:
                 self.strategy = 1
             else:
                 self.strategy = 0
 
     def play_strategy(self):
+        """
+        This function 'plays' the strategy by sending a string to the mediator in the game
+        :return:
+        """
+
+        # message that converges the strategy into a string
         row_number = str(int(self.strategy+1)) + str("th")
 
+        # sending the strategy to the mediator
         self.send('main', 'Alice will play the %s row' % row_number)
         self.send('strategy', self.strategy)
 
+        # storing the strategy as last played
         self.last_strategy = self.strategy
 
 
 class Bob(Agent):
     def on_init(self):
+        """
+        Initialize an agent (Bob) which commmunicates over two push messages
+        :return:
+        """
         self.bind('PUSH', alias='main')
         self.bind('PUSH', alias='strategy')
 
     def hello(self, name):
+        """
+        Hello from Bob!
+        :param name: Say the name of the other agent
+        :return:
+        """
         self.send('main', 'Hello to %s from Bob!' % name)
 
     def custom_log(self, message):
+        """
+        Standard message viewer
+        :param message: the input message that needs to be stated
+        :return:
+        """
         self.log_info('Received: %s' % message)
 
     def nash_strategy(self):
+        """
+        Set the strategy of Bob to Nash
+        :return:
+        """
         self.strategy = 1
 
     def random_strategy(self):
-        left_column = np.random.uniform()
+        """
+        Bob will play a random strategy
+        :return:
+        """
+        left_column = np.random.uniform()   # draw a random number from uniform distribution
 
+        # if the value drawn is higher than 0.5, we pick the left column, else the right one
         if left_column > 0.5:
             self.strategy = 0
         else:
             self.strategy = 1
 
     def cooperate_strategy(self):
+        """
+        This function makes Bob cooperate in the game
+        :return:
+        """
         self.strategy = 0
 
     def tit_for_tat_strategy(self, round_number):
+        """
+        Play a tit for tat strategy
+        :param round_number: The current round we are in with the game
+        :return:
+        """
+
+        # if we are in the first round, we will always cooperate
         if round_number == 1:
             self.strategy = 0
         else:
+            # if not, we base our strategy on the opponents last chosen strategy
             if self.opponent_strategy == 1:
                 self.strategy = 1
             else:
                 self.strategy = 0
 
     def receive_strategy(self, strategy):
+        """
+        This function stores the opponent strategy
+        :param strategy: strategy played by the opponent
+        :return:
+        """
         self.opponent_strategy = strategy
 
     def play_strategy(self):
-        column_number = str(int(self.strategy+1)) + str("th")
+        """
+        This function actually plays the strategy
+        :return:
+        """
+        column_number = str(int(self.strategy+1)) + str("th")   # convert strategy into a string
 
+        # send the message to the mediator
         self.send('main', 'Bob will play the %s column' % column_number)
         self.send('strategy', self.strategy)
 
+        # store the last strategy as being played
         self.last_strategy = self.strategy
 
 
 class Mediator(Agent):
     def on_init(self):
+        """
+        Initialize a mediator agent which binds with Alice and Bob
+        :return:
+        """
         self.bind('PUSH', alias='Alice_general')
         self.bind('PUSH', alias='Bob_general')
 
         self.bind('PUSH', alias='Alice_strategy_opponent')
         self.bind('PUSH', alias='Bob_strategy_opponent')
 
+        # set cumulative payoffs equal to zero
         self.cumulative_payoffs_p1 = 0
         self.cumulative_payoffs_p2 = 0
 
     def custom_log(self, message):
+        """
+        Standard message log function
+        :param message: The message that is being received
+        :return:
+        """
         self.log_info('Received: %s' % message)
 
     def receive_strategy_alice(self, strategy_alice):
+        """
+        Function that receives and stores the strategy played by Alice
+        :param strategy_alice: strategy played by alice
+        :return:
+        """
         self.alice_played = strategy_alice
 
     def receive_strategy_bob(self, strategy_bob):
+        """
+        Function that receives and stores the strategy played by Bob
+        :param strategy_bob: strategy played by Bob
+        :return:
+        """
         self.bob_played = strategy_bob
 
     def ask_action(self):
+        """
+        Function that asks Alice and Bob to take an action in the game
+        :return:
+        """
         self.send('Alice_general', 'Please state your action Alice')
         self.send('Bob_general', 'Please state your action Bob')
 
     def compute_result(self, round_number):
+        """
+        This function computes the result of the stage of the game after Alice and Bob have played a certain action
+        :param round_number: The round of the game we are in
+        :return:
+        """
+        # here we get the result of the game for both players
         result_p1 = game_p1[self.alice_played, self.bob_played]
         result_p2 = game_p2[self.alice_played, self.bob_played]
 
+        # we send a message to both with the result
         self.send('Alice_general', 'You have received a payoff last round of: %s' % str(result_p1))
         self.send('Bob_general', 'You have received a payoff last round of: %s' % str(result_p2))
 
+        # compute the cumulative payoff
         self.cumulative_payoffs_p1 += result_p1
         self.cumulative_payoffs_p2 += result_p2
 
+        # compute the average result for both
         self.average_result_p1 = self.cumulative_payoffs_p1/round_number
         self.average_result_p2 = self.cumulative_payoffs_p2/round_number
 
         time.sleep(0.5)
 
+        # send a message with the average reward
         self.send('Alice_general', 'You have an average reward of: %s' % str(self.average_result_p1))
         self.send('Bob_general', 'You have an average reward of: %s' % str(self.average_result_p2))
 
     def send_strategies(self):
+        """
+        Function that informs both players of the strategies of the opponent
+        :return:
+        """
         self.send('Alice_strategy_opponent', self.bob_played)
         self.send('Bob_strategy_opponent', self.alice_played)
 
 if __name__ == '__main__':
+    # this is the main game loop
 
+    # we set up a MAS with Alice, Bob and a Mediator
     ns = run_nameserver()
     alice = run_agent('Alice', base=Alice)
     bob = run_agent('Bob', base=Bob)
     mediator = run_agent('Mediator', base=Mediator)
 
+    #  Here below we do eight connection functions in order to stream messages
     alice.connect(mediator.addr('Alice_general'), handler='custom_log')
     bob.connect(mediator.addr('Bob_general'), handler='custom_log')
 
@@ -173,6 +306,7 @@ if __name__ == '__main__':
 
     print("")
 
+    # Here below we have the game loop, we loop from 0 to a total number of game rounds
     for i in np.arange(0, 100):
         print("")
         print("NEW ROUND, ROUND NUMBER:", i+1)
--- agents/agent_q_learning_easy.py
+++ agents/agent_q_learning_easy.py
@@ -1,26 +1,27 @@
-import time
 import numpy as np
-from osbrain import run_nameserver
-from osbrain import run_agent
-from osbrain import Agent
-from scipy.optimize import linprog
 import random
 
-game_p1 = np.array([3, 2])
+game_p1 = np.array([3, 2])  # we have reduced the minimax example to this game
 
-Q = np.ones((2))
-alpha = 1
-gamma = 0.99
-epsilon = 0.2
+# we initialize the following values
+Q = np.ones((2))        # Q-table
+alpha = 0.999           # learning rate
+gamma = 0.99            # discount factor
+epsilon = 0.2           # epsilon for the boundary of exploring or exploiting
 
 
 def determine_action():
-    if random.uniform(0, 1) < epsilon:
+    """
+    This function chooses to explore or exploit
+    :return:
+    """
+
+    if random.uniform(0, 1) < epsilon:  # if the sample drawn is lower than epsilon, we explore
         if random.uniform(0, 1) < 0.5:
             action = 0
         else:
             action = 1
-    else:
+    else:   # else we exploit
         if np.argmax(Q) == 0:
             action = 0
         else:
@@ -28,19 +29,18 @@ def determine_action():
 
     return action
 
-iterations = 1000000
+iterations = 3000   # total number of iterations
 
 
 for i in range(0, iterations):
-    # time.sleep(1)
+    # here is the general loop
     print("Now at iteration", i+1)
-    action = determine_action()
+    action = determine_action() # we select an action
     print("Player will now play action:", action)
-    # opponent_action = 1
-    # print("Opponent will now play action:", opponent_action)
-    reward = game_p1[action]
+
+    reward = game_p1[action]    # get the reward
     print("Current reward in the game is:", reward)
-    Q[action] = Q[action] + alpha * (reward + gamma * np.max(Q) - Q[action])
+    Q[action] = Q[action] + alpha * (reward + gamma * np.max(Q) - Q[action])    # update the Q-table
     print("New update to the Q table makes it: \n", Q)
     print("Currently at alpha", alpha)
     print("End of Q-learning")
